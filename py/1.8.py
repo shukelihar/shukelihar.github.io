77bff2f8225b48644e083d8a3e22268e47a66e3b
Ihar Shukel <38216117+shukelihar@users.noreply.github.com>
2021-03-20 13:45:08 +0300

    Add files via upload

---
+++ py/1.8.py
@@ -0,0 +1,5 @@
+X = int(input())
+H = X // 60
+M = X % 60
+print(H)
+print(M)
---
+++ py/test.py
@@ -0,0 +1,9 @@
+# startBalance = int(input("Enter start balance "))
+# percent = int(input("Enter percent to day "))
+# days = int(input("Enter days "))
+
+# for i in range(days):
+#     startBalance = startBalance + startBalance / 100 * percent
+#     print(i)
+#     print(startBalance)
+
